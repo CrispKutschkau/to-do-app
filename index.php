8d95fa69e376a10aa9d1e035701562e23d408943
CrispKutschkau <kkutschkau@crispnow.com>
2023-06-26 16:22:00 -0600

    Got rid of unneeded file structure

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do App</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="php-tutorial/styles/index.css">
+    <link rel="stylesheet" href="php-tutorial/styles/reset.css">
 </head>
 <body>
-    
+    <div id="app-header">
+        <h1 id="app-title">To-Do List</h1>
+    </div>
 </body>
 </html>
