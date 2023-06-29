59e67b79cd51f215c44e21e39afe0cf58ffe4745
CrispKutschkau <kkutschkau@crispnow.com>
2023-06-29 15:58:16 -0600

    design aspects done

--- index.php
+++ index.php
@@ -8,8 +8,36 @@
     <link rel="stylesheet" href="php-tutorial/styles/reset.css">
 </head>
 <body>
-    <div id="app-header">
-        <h1 id="app-title">To-Do List</h1>
+    <div id="app">
+        <div id="app-header">
+            <h1 id="app-title">To-Do List</h1>
+        </div>
+        <div id="app-body">
+            <div id="list-box">
+                <span id="list-title">
+                    Tasks
+                </span>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+                <h3>Testing</h3>
+            </div>
+        </div>
+        <div id="app-footer">
+            All icons taken from: <a href="https://www.flaticon.com/free-icons/checkbox" title="checkbox icons">Checkbox icons created by Kushal Jain - Flaticon</a>
+        </div>
     </div>
 </body>
 </html>
