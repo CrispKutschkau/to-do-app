2a21846696f199e3e2d1ecabfaaa931885ebbd76
CrispKutschkau <kkutschkau@crispnow.com>
2023-07-17 14:08:45 -0600

    Added the ability to take user input for the to do list.

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do App</title>
+    <title>To-Do List</title>
     <link rel="stylesheet" href="php-tutorial/styles/index.css">
     <link rel="stylesheet" href="php-tutorial/styles/reset.css">
+    <link rel="icon" type="image/x-icon" href="php-tutorial/static/icons/checklist.svg">
 </head>
 <body>
     <div id="app">
@@ -13,30 +14,35 @@
             <h1 id="app-title">To-Do List</h1>
         </div>
         <div id="app-body">
-            <div id="list-box">
-                <span id="list-title">
+            <div class="container-box">
+                <span class="container-box-title">
                     Tasks
                 </span>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
-                <h3>Testing</h3>
+                <div id="task-list-body">
+                    <?php 
+                        $newTask = $_POST['new-task'];
+                        $taskList = array();
+
+                        $taskList[] = $newTask;
+
+                        foreach($taskList as $task){
+                            echo "<li>$task</li>";
+                        };
+                    ?>
+                </div>
+            </div>
+            <div class="container-box">
+                <span class="container-box-title">
+                    New Task
+                </span>
+                <form id="new-task-form" method="post">
+                    <input type="text" name="new-task" id="task-input-box">
+                    <input type="submit" id="task-submit-btn" value="ADD">
+                </form>
             </div>
         </div>
         <div id="app-footer">
-            All icons taken from: <a href="https://www.flaticon.com/free-icons/checkbox" title="checkbox icons">Checkbox icons created by Kushal Jain - Flaticon</a>
+            This is where a good footer would go with some link or something cool like that</a>
         </div>
     </div>
 </body>
